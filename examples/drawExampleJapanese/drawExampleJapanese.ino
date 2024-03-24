d352d1d1e5afe1b25c338ded31e76d4888058eaf
William Edmisten <wcedmisten@gmail.com>
2024-03-24 16:21:49 -0400

    Add more japanese vocab

--- examples/drawExampleJapanese/drawExampleJapanese.ino
+++ examples/drawExampleJapanese/drawExampleJapanese.ino
@@ -13,6 +13,22 @@ uint8_t *framebuffer = NULL;
 int cursor_x = 400;
 int cursor_y = 250;
 
+char *vocab[][2] = {
+  {"こんにちは", "hello"},
+  {"たべもの", "food"},
+  {"ひとつ", "one (thing)"},
+  {"ふたつ", "two (things)"},
+  {"えん", "yen"},
+  {"かね", "money"},
+  {"これ", "this, this one"},
+  {"すいようび", "Wednesday"},
+  {"あれ", "that over there"},
+};
+
+int NUM_VOCAB = 9;
+
+int vocab_index = 1;
+
 void setup()
 {
     Serial.begin(115200);
@@ -33,27 +49,12 @@ void setup()
     // epd_poweroff();
 
     // epd_poweron();
-
-    char *vocab[][2] = {
-      {"こんにちは", "hello"},
-      {"たべもの", "food"},
-    };
-
-    int index = 1;
-
-    // write_string((GFXfont *)&FiraSans, "Hello!", &cursor_x, &cursor_y, NULL);
-    write_string((GFXfont *)&demo, vocab[index][0], &cursor_x, &cursor_y, NULL);
-    cursor_x = 400;
-    cursor_y = 400;
-    write_string((GFXfont *)&demo, vocab[index][1], &cursor_x, &cursor_y, NULL);
-
-    epd_draw_grayscale_image(epd_full_screen(), framebuffer);
-    epd_poweroff();
 }
 
 void loop()
 {
-    // epd_poweron();
+    epd_poweron();
+    epd_clear();
 
     // write_string((GFXfont *)&FiraSans, "Hello World", &cursor_x, &cursor_y, NULL);
     // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
@@ -81,5 +82,22 @@ void loop()
 
     // memset(framebuffer, 0xFF, EPD_WIDTH * EPD_HEIGHT / 2);
     // epd_clear();
-    // epd_poweroff();
+
+    // write_string((GFXfont *)&FiraSans, "Hello!", &cursor_x, &cursor_y, NULL);
+    write_string((GFXfont *)&demo, vocab[vocab_index][0], &cursor_x, &cursor_y, NULL);
+    cursor_x = 400;
+    cursor_y = 400;
+    write_string((GFXfont *)&demo, vocab[vocab_index][1], &cursor_x, &cursor_y, NULL);
+
+    epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+
+    epd_poweroff();
+
+    vocab_index += 1;
+    vocab_index = vocab_index % NUM_VOCAB;
+
+    cursor_x = 400;
+    cursor_y = 250;
+
+    delay(10000);
 }
